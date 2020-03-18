9be4a1e56c09afaba577c5cb127892b46375cfcf
bailey-hill <bhill1@mail.sfsu.edu>
2020-03-18 16:43:21 -0700

    styled form

--- client/components/create-post.jsx
+++ client/components/create-post.jsx
@@ -44,30 +44,30 @@ class CreatePost extends React.Component {
 
   render() {
     return (
-      <div className="d-flex align-items-start flex-column">
+      <div className="w-100 d-flex align-items-center flex-column">
         <nav className="d-flex justify-content-space-between navbar navbar-expand-lg navbar-light ">
-          <h2 className="">Create Post</h2>
-          <h2 onClick={this.goToDashboard}><i className="fas fa-arrow-left"></i></h2>
+          <h2 className="font-weight-bolder generalText">Create Post</h2>
+          <h2 onClick={this.goToDashboard}>Back<i className="fas fa-arrow-left"></i></h2>
         </nav>
         <form className="d-flex justify-content-center w-100 align-items-center
         flex-column form" onSubmit={this.handleSubmit}>
           <div className="w-100">
-            <div className="mr-1 mb-1 mt-1">Post body</div>
-            <textarea className="form-control" type="text" id="postBody"
+            <div className="font-weight-bolder generalText ml-1 mb-1 mt-1">Post body</div>
+            <textarea className="generalText form-control" type="text" id="postBody"
               placeholder="post body"
               onChange={this.handleChange} />
           </div>
           <div className="w-100">
-            <div className="mr-1 mb-1 mt-1">Post tags</div>
-            <textarea className="form-control" type="text" id="postTags"
+            <div className="font-weight-bolder generalText ml-1 mb-1 mt-1">Post tags</div>
+            <textarea className="generalText form-control" type="text" id="postTags"
               placeholder="post tags"
               onChange={this.handleChange} />
           </div>
-          <div className="d-flex justify-content-center createPostButton">
+          <div className="d-flex flex-row w-100 justify-content-end createPostButton">
             <button className="mt-3 mr-2 generalText btn-light font-weight-bolder rounded-lg loginButton"
               onSubmit={this.handleSubmit}
               type="submit" value="Submit">Create Post</button>
-            <input className="mt-3 btn  generalText font-weight-bolder
+            <input className="mt-3 btn generalText font-weight-bolder
             rounded-lg loginButton" type="reset" value="Cancel" />
           </div>
         </form>
