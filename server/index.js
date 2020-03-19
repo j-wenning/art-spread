f7bdb2a1be09f4afc4fb1b5d76951f4a0af12d14
justin-wenning <wenning.justin@gmail.com>
2020-03-19 16:16:00 -0700

    Created middleware for getting profile id list Created middleware for getting a specific profile

--- server/index.js
+++ server/index.js
@@ -13,17 +13,45 @@ app.use(sessionMiddleware);
 
 app.use(express.json());
 
-app.get('/api/user/:userId', (req, res, next) => {
-  const sql = `
-    select "u"."username",
-           "p"."profileName",
-           "p"."avatarPath"
-      from "profiles" as "p"
-      join "users" as "u" using ("userId")
-    where "p"."userId" = 1;
-  `;
-  db.query(sql)
-    .then(result => res.json(result.rows[0]))
+app.get('/api/profiles/:userId', (req, res, next) => {
+  const userId = req.params.userId;
+  if (userId < 1) throw new ClientError('Invalid userId.', 400);
+  db.query(`
+    SELECT "profileId"
+      FROM "profiles"
+     WHERE "userId" = $1;
+  `, [userId])
+    .then(result => {
+      if (result.rows.length === 0) {
+        return db.query(`
+        SELECT *
+          FROM "users"
+         WHERE "userId" = $1;
+      `, [userId]);
+      }
+      res.json(result.rows);
+    })
+    .then(result => {
+      if (result.rows.length === 0) throw new ClientError('User does not exist.', 404);
+      else res.json([]);
+    })
+    .catch(err => next(err));
+});
+
+app.get('/api/profile/:profileId', (req, res, next) => {
+  const profileId = req.params.profileId;
+  if (profileId < 1) throw new ClientError('Invalid profileId.', 400);
+  db.query(`
+    SELECT "profileId",
+           "name",
+           "imagePath"
+      FROM "profiles"
+     WHERE "profileId" = $1;
+  `, [profileId])
+    .then(result => {
+      if (result.rows.length === 0) throw new ClientError('Profile does not exist.', 404);
+      res.json(result.rows);
+    })
     .catch(err => next(err));
 });
 
