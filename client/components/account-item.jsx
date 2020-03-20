1e7648642a5a38ad99b64ae7a38c797396acca8c
bailey-hill <bhill1@mail.sfsu.edu>
2020-03-20 13:12:35 -0700

    finished edits on user can adjust settings

--- client/components/account-item.jsx
+++ client/components/account-item.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 class AccountItem extends React.Component {
   render() {
     return (
-      <div id={this.props.accountId} className="card d-flex align-items-center">
-        <div className=" d-flex flex-row">
-          <p className="card-title">{this.props.name}</p>
+      <div id={this.props.accountId} className="mt-2 mb-2 card d-flex align-items-center">
+        <div className="d-flex flex-row">
+          <div className="mt-2 mb-2">{this.props.name}</div>
         </div>
       </div>
     );
