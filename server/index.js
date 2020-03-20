61f22f5bd438d35db325a1a275aa27806531987f
justin-wenning <wenning.justin@gmail.com>
2020-03-19 21:13:21 -0700

    Refactored get middleware: profiles now returns all profile credentials Refactored get middleware: posts now return all post-related data Completed send accounts middleware creation

--- server/index.js
+++ server/index.js
@@ -15,44 +15,37 @@ app.use(express.json());
 
 app.get('/api/profiles/:userId', (req, res, next) => {
   const userId = Number(req.params.userId);
+
   if (!userId) throw new ClientError('Requires userId', 400);
   if (userId < 1) throw new ClientError('Invalid userId.', 400);
   db.query(`
-    SELECT "profileId"
-      FROM "profiles"
-     WHERE "userId" = $1;
+      SELECT "profileId",
+             "name",
+             "imgPath"
+        FROM "profiles"
+       WHERE "userId" = $1
+    ORDER BY "profileId";
   `, [userId])
     .then(result => {
-      if (result.rows.length === 0) {
-        return db.query(`
-        SELECT *
-          FROM "users"
-         WHERE "userId" = $1;
-      `, [userId]);
-      }
-      res.json(result.rows);
-    })
-    .then(result => {
-      if (result.rows.length === 0) throw new ClientError('User does not exist.', 404);
-      else res.json([]);
+      res.json(result.rows || []);
     })
     .catch(err => next(err));
 });
 
-app.get('/api/profile/:profileId', (req, res, next) => {
+app.get('/api/accounts/:profileId', (req, res, next) => {
   const profileId = Number(req.params.profileId);
-  if (!profileId) throw new ClientError('Requires profileId', 400);
-  else if (profileId < 1) throw new ClientError('Invalid profileId.', 400);
+
   db.query(`
-    SELECT "profileId",
-           "name",
-           "imgPath"
-      FROM "profiles"
-     WHERE "profileId" = $1;
+      SELECT "a"."accountId",
+             "a"."name",
+             "a"."token"
+        FROM "account-profile-links"
+        JOIN "accounts" AS "a" USING ("accountId")
+       WHERE "profileId" = $1
+    ORDER BY "linkId";
   `, [profileId])
     .then(result => {
-      if (result.rows.length === 0) throw new ClientError('Profile does not exist.', 404);
-      res.json(result.rows[0]);
+      res.json(result.rows || []);
     })
     .catch(err => next(err));
 });
@@ -70,9 +63,9 @@ app.get('/api/posts/:profileId', (req, res, next) => {
   else if (profileId < 1) throw new ClientError('Invalid profileId', 400);
   db.query(`
       SELECT "postId",
-             "postBody",
-             "postTags",
-             "imgPath",
+             "body",
+             "tags",
+             "imgPath"
         FROM "posts"
        WHERE "postId" >= $1 AND "profileId" = $2
     ORDER BY "postId" DESC
@@ -80,13 +73,13 @@ app.get('/api/posts/:profileId', (req, res, next) => {
   `, [postId, profileId, postCount])
     .then(result => {
       if (result.rows.length === 0) throw new ClientError('Posts do not exist.', 404);
-      res.json(result);
+      res.json(result.rows);
     })
     .catch(err => next(err));
 });
 
 // user can post profile data
-app.get('/api/profiles', (req, res, next) => {
+app.post('/api/profiles', (req, res, next) => {
   const { profileId, name, imagePath, userId } = req.body;
   const values = [profileId, name, imagePath, userId];
 
@@ -99,17 +92,6 @@ app.get('/api/profiles', (req, res, next) => {
 app.get('/api/publications', (req, res, next) => {
   const { publicationId, postLink, accountId, postId } = req.body;
   const values = [publicationId, postLink, accountId, postId];
-
-  const sql = ' ';
-  db.query(sql, values)
-    .then(result => res.json(result.rows[0]))
-    .catch(err => next(err));
-});
-
-// send associated Front end can retrieve accounts associated with a specified profile.
-app.get('/api/accounts', (req, res, next) => {
-  const { accountId, name, accountToken, userId } = req.body;
-  const values = [accountId, name, accountToken, userId];
 
   const sql = ' ';
   db.query(sql, values)
