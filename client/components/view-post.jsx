87ebf8154ec3100a208ab14992c226b49e021e32
bailey-hill <bhill1@mail.sfsu.edu>
2020-03-20 16:18:00 -0700

    created post body and post tags box

--- client/components/view-post.jsx
+++ client/components/view-post.jsx
@@ -32,7 +32,7 @@ export default class ViewPost extends React.Component {
         <div className="d-flex justify-content-center">
           <img className="preview-image-lg" src="./assets/images/default-image.svg" alt=""/>
         </div>
-        <div className="container-fluid mt-3 mb-3">
+        <div className="container-fluid mt-3 mb-3 ml-1 mr-1">
           <div className="horizontal-list overflow-auto row row-horizon flex-row
           flex-nowrap align-items-center">
             <div className="col-4">
@@ -53,6 +53,16 @@ export default class ViewPost extends React.Component {
             <div className="col-4">
               <div className="text-custom-primary">stats</div>
             </div>
+          </div>
+        </div>
+        <div className="w-100">
+          <div className="post-body p-2 text-custom-primary ml-1 mb-1 mt-1">
+            Post body
+          </div>
+        </div>
+        <div className="w-100 mt-3 mb-3">
+          <div className="post-tags p-2 text-custom-primary ml-1 mb-1 mt-1">
+            Post tags
           </div>
         </div>
       </div>
