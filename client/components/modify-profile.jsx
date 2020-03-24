72bdff2f31d7520791618007a94a85112cec3b20
bailey-hill <bhill1@mail.sfsu.edu>
2020-03-24 12:40:42 -0700

    created text fields and form for bio and handle

--- client/components/modify-profile.jsx
+++ client/components/modify-profile.jsx
@@ -23,14 +23,42 @@ class ModifyProfile extends React.Component {
     const pfp = this.state.profile.picture || './assets/images/default-profile.svg';
     return (
       <div>
-        <div className="row d-flex justify-content-center">
-          <img className="profile-picture mr-2" src={pfp} alt="" />
-          <label htmlFor="image-file" className="custom-file-upload btn btn-custom
+        <form encType="multipart/form-data"
+          className="form" id={this.props.profileId}
+          onSubmit={this.handleSubmit}>
+          <div className="row d-flex justify-content-center">
+            <img className="profile-picture mr-2" src={pfp} alt="" />
+            <label htmlFor="image-file" className="custom-file-upload btn btn-custom
           text-custom-primary avatar-btn mt-4 ml-2 w-50">
             Upload avatar</label>
-          <input onClick={this.handleAvatar}
-            id="image-file" type="file" className="imageInput" />
-        </div>
+            <input onClick={this.handleAvatar}
+              id="image-file" type="file" className="imageInput" />
+          </div>
+          <div className="mt-3 w-100">
+            <div className="text-custom-primary ml-1 mb-1 mt-1">
+              Vanity handle
+            </div>
+            <textarea
+              className="generalText form-control"
+              type="text"
+              id="vanityHandle"
+              placeholder="vanity handle"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="w-100">
+            <div className="text-custom-primary ml-1 mb-1 mt-1">
+              Bio
+            </div>
+            <textarea
+              className="generalText form-control"
+              type="text"
+              id="bio"
+              placeholder="bio"
+              onChange={this.handleChange}
+            />
+          </div>
+        </form>
       </div>
     );
   }
