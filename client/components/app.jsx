9a448e3e7cef7be94c5f9981b2f7646419e96d9b
bailey-hill <bhill1@mail.sfsu.edu>
2020-03-24 17:01:38 -0700

    included delete button and icons

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -13,7 +13,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       view: {
-        name: 'modifyProfile',
+        name: 'login',
         params: {}
       }
     };
