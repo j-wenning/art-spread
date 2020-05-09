44acfa46c3301a55cebf37ae2667bcc1d24f1a71
justin-wenning <wenning.justin@gmail.com>
2020-05-08 17:05:14 -0700

    Updated fetch queue to handle multiple platforms at once to accomodate joint fetches

--- server/fetch-queue.js
+++ server/fetch-queue.js
@@ -1,53 +1,69 @@
 module.exports = class FetchQueue {
   constructor() {
     this.items = [];
-    this.platforms = ['reddit', 'imgur'];
-    this.waitTimes = new Array(this.platforms.length).fill(0);
+    this.knownPlatforms = ['reddit', 'imgur'];
+    this.waitTimes = new Array(this.knownPlatforms.length).fill(0);
+    this.expiry = 3 * 60 * 1000;
+    this.offset = 10 * 1000;
   }
 
-  getWait(platform) {
-    const { waitTimes, platforms } = this;
-    return waitTimes[platforms.indexOf('platform')];
+  getWait(platforms) {
+    return platforms.map(platform =>
+      this.waitTimes[this.knownPlatforms.indexOf(platform)]
+    );
   }
 
   setWait(platform, val) {
-    const { waitTimes, platforms } = this;
-    waitTimes[platforms.indexOf('platform')] = val;
+    this.waitTimes[this.knownPlatforms.indexOf(platform)] = val;
   }
 
   enqueue(item) {
-    const { items, platforms, dequeue } = this;
-    const { platform, action } = item;
-    const inTimestamp = Date.now();
-    if (!platform || typeof platform !== typeof String()) {
-      return console.error('Invalid platform');
-    } else if (platforms.includes(platform)) {
-      return console.error('Platform not recognized');
+    const { platforms, action } = item;
+    const timestamp = Date.now();
+    const platformFilter = platforms.filter(platform =>
+      !this.knownPlatforms.includes(platform)
+    );
+    if (platforms.length === 0) {
+      return console.error('Requires platform(s)');
+    } else if (platformFilter.length !== 0) {
+      return console.error('Unrecognized platform:', platformFilter[0]);
     } else if (!action || typeof action !== typeof Function) {
       return console.error('Invalid action');
     }
-    item.inTimestamp = inTimestamp;
-    items.push(item);
-    if (items.length === 1) {
-      dequeue();
+    item.timestamp = timestamp;
+    this.items.push(item);
+    if (this.items.length === 1) {
+      this.dequeue();
     }
   }
 
   dequeue() {
-    const { items, getWait, dequeue } = this;
-    const { platform, action } = items[0];
-    if (getWait(platform) < Date.now()) {
-      items.shift();
-      action()
-        .then(params => {
-          const { requests, time } = params;
-          if (requests <= 10) {
-            this.setWait(platform, Date.now() + 10000 + time * 1000);
-          }
-          if (items.length === 1) {
-            dequeue();
-          }
-        }).catch(err => console.error(err));
-    } else items.push(items.shift());
+    setTimeout(() => {
+      const { platforms, action, timestamp } = this.items[0];
+      if (this.getWait(platforms).reduce((a, b) => a > b ? a : b, 0) < Date.now()) {
+        this.items.shift();
+        action()
+          .then(params => {
+            params.forEach(param => {
+              const { platform, requests, time } = param;
+              if (!this.knownPlatforms.includes(platform)) {
+                return console.error('Unrecognized platform:', platform);
+              }
+              if (requests <= 15) {
+                this.setWait(platform, Date.now() + this.offset + time);
+              }
+            });
+            if (this.items.length >= 1) {
+              this.dequeue();
+            }
+          }).catch(err => console.error(err));
+      } else if (Date.now() - timestamp > this.expiry) {
+        this.items.shift();
+        this.dequeue();
+      } else {
+        this.items.push(this.items.shift());
+        this.dequeue();
+      }
+    }, 2);
   }
 };
