233d68d516e3c0db9cb7a972d0a21b58398a6980
justin-wenning <wenning.justin@gmail.com>
2020-05-12 15:23:29 -0700

    Updated switch profile module

--- client/components/switch-profile.jsx
+++ client/components/switch-profile.jsx
@@ -1,93 +1,66 @@
 import React from 'react';
-import ProfileItem from './profile-item';
 
-export default class Settings extends React.Component {
+export default class SwitchProfile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      profiles: []
-    };
-    this.getProfiles = this.getProfiles.bind(this);
-    this.deleteProfile = this.deleteProfile.bind(this);
-    this.createProfile = this.createProfile.bind(this);
+    this.state = { profiles: [] };
   }
 
-  getProfiles() {
-    fetch('/api/profiles')
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ profiles: data });
-      });
-  }
-
-  deleteProfile() {
-    const eventTarget = event.target.id;
-    fetch(`/api/profiles/${eventTarget}`, {
-      method: 'DELETE'
-    })
-      .then(() => {
-        function test(profile) {
-          return profile.id !== Number(eventTarget);
-        }
-        const newArr = this.state.profiles.filter(test);
-        this.setState({
-          profiles: newArr
-        });
-      });
-  }
-
-  createProfile() {
-    fetch('/api/profiles', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        profileId: 'admin',
-        name: 'admin',
-        imagePath: 'admin',
-        userId: 'admin'
-      })
-    }).then(response => response.json())
-      .then(data => {
-        this.setState({
-          profiles: this.state.profiles.concat(data)
-        });
-      });
-    this.goToModifyProfile();
-  }
-
-  goToModifyProfile(event) {
-    this.props.setView('modifyProfile', {});
+  deleteProfile(index) {
+    this.props.deleteProfile(index);
+    this.setState(state => {
+      state.profiles.splice(index, 1);
+      return state;
+    });
   }
 
   componentDidMount() {
-    this.getProfiles();
+    this.props.fetchProfiles()
+      .then(data => this.setState({ profiles: data }))
+      .catch(err => console.error(err));
   }
 
   render() {
-    return (
-      <div>
-        <div className="row">
-          <div className="col">
-            <div className="prof-list overflow-auto">
-              {this.deleteProfile && this.getProfiles && this.state.profiles.map(profile => {
-                return (
-                  <ProfileItem
-                    key={profile.profileId}
-                    profilePic={profile.profilePic}
-                    name={profile.name}
-                    id={profile.profileId}
-                  />);
-              })
-              }
-            </div>
-          </div>
+    const profiles = this.state.profiles.map((item, index) => (
+      <div key={index} className="profile col-12">
+        <div className="row p-2">
+          <button
+            onClick={() => this.props.setCurrentProfile(index)}
+            type="button"
+            className="col btn btn-custom text-primary border-0 mr-2">
+            {item.name}
+          </button>
+          <button
+            onClick={() => this.deleteProfile(index)}
+            type="button"
+            className="btn btn-custom-menu border-0">
+            <i className="fas fa-trash text-danger" />
+          </button>
         </div>
-        <div className="row">
-          <div className="mt-2 d-flex flex-row w-100 justify-content-center">
-            <button onClick={this.createProfile} className="btn btn-custom text-custom-primary">
-              Create Profile
-            </button>
-          </div>
+      </div>
+    ));
+    const hasNoProfiles = !this.state.profiles.length;
+    return (
+      <div className="switch-profile container text-center">
+        <button
+          onClick={() => {
+            this.props.createProfile();
+            this.props.viewModifyProfile();
+          }}
+          type="button"
+          className="btn btn-custom text-capitalize">
+          create profile
+        </button>
+        <div className="vh-60 overflow-auto bg-secondary py-4 mt-2">
+          {profiles}
+          {hasNoProfiles && (
+            <div className="col-12">
+              <p
+                className="bg-primary rounded text-primary text-capitalize py-2">
+                no profiles
+              </p>
+            </div>
+          )}
         </div>
       </div>
     );
